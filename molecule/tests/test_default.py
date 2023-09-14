508e52e2f279a1e8936e381099de39d3ef4248bb
Tony Nyurkin <ptqa.mail@gmail.com>
2023-09-15 03:05:07 +0400

    add loki promtail test

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -1,4 +1,6 @@
 import os
+import time
+import json
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -39,3 +41,13 @@ def test_is_alertmanager_container(host):
 def test_is_promtail_container(host):
     container_promtail = host.docker.get_containers(status="running", name='gc-promtail')
     assert container_promtail
+
+def test_is_promtail_journal(host):
+    # Push data to journald
+    host.run('echo "testing promtail -> loki pipeline" | systemd-cat -t mytestingservice')
+    # Wait for promtail to push data to loki
+    time.sleep(5)
+    # Check if loki has a data from journald
+    cmd = host.run('curl -G 127.1:3100/loki/api/v1/label/syslog_identifier/values')
+    loki_data = json.loads(cmd.stdout)
+    assert 'mytestingservice' in loki_data['data']
